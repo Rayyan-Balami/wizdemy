20d1ec67402f2d43deec9a1b5c950d44457ef5e6
Rayyan-Balami <visionrayyan60517@gmail.com>
2024-05-01 22:45:16 +0545

    Update meta tags for Open Graph and Twitter in Header.php and manifest.json

--- app/views/partials/Header.php
+++ app/views/partials/Header.php
@@ -7,11 +7,26 @@
   <meta name="description" content="<?= $pageDescription ?? SITE_DESCRIPTION ?>" />
   <meta name="author" content="<?= SITE_AUTHOR ?>" />
   <meta name="keywords" content="<?= $pageKeywords ?? SITE_KEYWORDS ?>" />
-  <meta name="theme-color" content="currentColor" />
+  <meta name="theme-color" content="#ffffff" />
   <link rel="icon" href="/assets/icons/favicon.ico" />
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-icon-180x180.png" />
   <link rel="manifest" href="/assets/manifest.json" />
+
+  <!-- Open Graph / Facebook -->
   <meta property="og:image" content="<?= $ogImage ?? '/assets/images/og-thumbnail.png' ?>" />
+  <meta property="og:title" content="<?= $pageTitle ?>" />
+  <meta property="og:description" content="<?= $pageDescription ?? SITE_DESCRIPTION ?>" />
+  <meta property="og:url" content="<?= SITE_DOMAIN . $_SERVER['REQUEST_URI'] ?>" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content="<?= SITE_NAME ?>" />
+
+  <!-- Twitter -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@BalamiRayyan" />
+  <meta name="twitter:creator" content="@BalamiRayyan" />
+  <meta name="twitter:title" content="<?= $pageTitle ?>" />
+  <meta name="twitter:description" content="<?= $pageDescription ?? SITE_DESCRIPTION ?>" />
+  <meta name="twitter:url" content="<?= SITE_DOMAIN . $_SERVER['REQUEST_URI'] ?>" />
   <meta name="twitter:image" content="<?= $ogImage ?? '/assets/images/og-thumbnail.png' ?>" />
   <title>
     <?= $pageTitle ?>
